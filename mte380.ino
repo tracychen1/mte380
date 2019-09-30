e40ef5e6d138444e8f1bb72b8db8778015f2d60a
tracywhchen <tracywhchen@hotmail.com>
2019-09-30 10:46:25 -0400

    more debugging but still doesn't work

--- mte380.ino
+++ mte380.ino
@@ -19,14 +19,44 @@ class MyServo {
 
 // ------------------------------------
 
-MyServo servo1(9);
+MyServo servo1(11);
+int joyPin1 = 7;
+int joyPin2 = 8;
+int jsval1 = 0;
+int jsval2 = 0;  
 
-void setup() {}
+// servo to sweep 180
+Servo servo9;
+int pos = 0;
+
+void setup() {
+  Serial.begin(9600);
+//  servo9.attach(9); // servo to sweep 180
+ }
 
 void loop() {
   // something is a little f'ed up with this code but it can move the servos
-  servo1.moveCW(90, 5);
+  jsval1 = analogRead(joyPin1);
+  delay(100);
+  jsval2 = analogRead(joyPin2);
+
+  Serial.println(jsval1);
+  
+
+//  // servo to sweep 180
+//  for(pos = 0; pos < 180; pos += 1) {
+//    servo9.write(pos);
+//  }
+//  delay(2000);
+//  for(pos = 180; pos>=1; pos -= 1) {
+//    servo9.write(pos);
+//  } 
+//  delay(1000); 
+  
   servo1.moveCCW(90, 5);
+  delay(5000); // need this
+  servo1.moveCW(90, 5);
+  delay(1000); // need this
 }
 
 // ------------------------------------
@@ -36,18 +66,20 @@ MyServo::MyServo(int pin) {
   theServo.attach(m_pin);
 }
 
-void MyServo::moveCW(int desiredAngle, int speedStep) {
-  for (int newAngle = m_angle; newAngle < desiredAngle; newAngle += speedStep) {                               
-      theServo.write(newAngle);             
+void MyServo::moveCCW(int desiredAngle, int speedStep) {
+  for (m_angle = 0; m_angle <= desiredAngle; m_angle += speedStep) {
+    theServo.write(m_angle);
   }
-  m_angle += desiredAngle;
+//  Serial.println("Move CW and now m_angle is");
+//  Serial.println(m_angle);
 }
 
-void MyServo::moveCCW(int desiredAngle, int speedStep) {
-  for (int newAngle = m_angle; newAngle >= desiredAngle; newAngle -= speedStep) {                               
-      theServo.write(newAngle);         
+void MyServo::moveCW(int desiredAngle, int speedStep) {
+  for (m_angle = desiredAngle; m_angle >= 0; m_angle -= speedStep) {
+    theServo.write(m_angle);
   }
-  m_angle -= desiredAngle;
+//  Serial.println("Move CCW and now m_angle is");
+//  Serial.println(m_angle);
 }
 
 int MyServo::getAngle() {
