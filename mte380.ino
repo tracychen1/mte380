e50f04aaf3a7d06f946b5e81801290a4c25a5517
tracywhchen <tracywhchen@hotmail.com>
2019-10-01 22:00:24 -0400

    hard coded all the L/R/U/D movements for all servos

--- mte380.ino
+++ mte380.ino
@@ -1,87 +1,168 @@
 #include "Servo.h"
 
-class MyServo {
-  private:
-    Servo theServo;
-    int m_angle = 0;
-    int m_pin = 999;
+// joystick
+int jsx = 0;
+int jsy = 0;  
+int bound1 = 300;
+int bound2 = 800;
 
-  public:
-    MyServo(int pin);
+// servos
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
 
-    // assume that we won't be pushing the servo beyond its limits
-    void moveCW(int desiredAngle, int speedStep);
-    void moveCCW(int desiredAngle, int speedStep);
+// positions
+// 0 position is when it can't turn CW anymore
+// tilt is for 5 degrees
+// flat, forward, backward, left, right 
+int pos1[] = {102, 88, 116, 93, 113};
+int pos2[] = {102, 92, 117, 113, 93};
+int pos3[] = {109, 104, 109, 127, 93};
+int pos4[] = {109, 128, 97, 116, 104};
+int pos5[] = {109, 123, 96, 104, 116};
+int pos6[] = {109, 107, 118, 93, 127};
 
-    // getters
-    int getAngle();
-};
+// to tilt platform by 1 degree in forward/back
+// pos is forward
+double forBack[] = {2.796, 2.519, 0.405, -3.122, -2.723, 1.082};
+
+// to tilt platform by 1 degree in left/right
+// positive is 
+double leftRight[] = {2.008, -2.011, -3.399, -1.155, 1.156, 3.405};
 
 // ------------------------------------
-
-MyServo servo1(11);
-int joyPin1 = 7;
-int joyPin2 = 8;
-int jsval1 = 0;
-int jsval2 = 0;  
-
-// servo to sweep 180
-Servo servo9;
-int pos = 0;
 
 void setup() {
   Serial.begin(9600);
-//  servo9.attach(9); // servo to sweep 180
+  
+  servo1.attach(3);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(9);
+  servo5.attach(10);
+  servo6.attach(11);
+  servo6.write(pos6[0]);
  }
 
 void loop() {
-  // something is a little f'ed up with this code but it can move the servos
-  jsval1 = analogRead(joyPin1);
+  // read the pins on the joystick
+  // home position for joystick is at 511 (1023/2)
+  jsx = analogRead(A0);
   delay(100);
-  jsval2 = analogRead(joyPin2);
+  jsy = analogRead(A1);
+  delay(100);
 
-  Serial.println(jsval1);
+  // we are just considering moving the joystick in 4 directions
+  // so don't move on the diagonals
+  bool moved = false;
+  double count=0;
+  double x = 0, y = 0; // 0: no change | positive: right/up | negative: left/down
+
+  // LEFT
+  while (jsx < bound1) {
+    x = -1;
+    servo1.write(pos1[0] + leftRight[0]*x*count);
+    servo2.write(pos2[0] + leftRight[1]*x*count);
+    servo3.write(pos3[0] + leftRight[2]*x*count);
+    servo4.write(pos4[0] + leftRight[3]*x*count);
+    servo5.write(pos5[0] + leftRight[4]*x*count);
+    servo6.write(pos6[0] + leftRight[5]*x*count);
+
+    delay(100);
+    count++;
+    jsx = analogRead(A0);
+    moved = true;
+  } 
+
+  // RIGHT
+  while (jsx > bound2) {
+    x = 1;
+    servo1.write(pos1[0] + leftRight[0]*x*count);
+    servo2.write(pos2[0] + leftRight[1]*x*count);
+    servo3.write(pos3[0] + leftRight[2]*x*count);
+    servo4.write(pos4[0] + leftRight[3]*x*count);
+    servo5.write(pos5[0] + leftRight[4]*x*count);
+    servo6.write(pos6[0] + leftRight[5]*x*count);
+
+    delay(100);
+    count++;
+    jsx = analogRead(A0);
+    moved = true;
+  } 
   
+  // DOWN
+  while (jsy < bound1) {
+    y = -1;
+    servo1.write(pos1[0] + forBack[0]*y*count);
+    servo2.write(pos2[0] + forBack[1]*y*count);
+    servo3.write(pos3[0] + forBack[2]*y*count);
+    servo4.write(pos4[0] + forBack[3]*y*count);
+    servo5.write(pos5[0] + forBack[4]*y*count);
+    servo6.write(pos6[0] + forBack[5]*y*count);
 
-//  // servo to sweep 180
-//  for(pos = 0; pos < 180; pos += 1) {
-//    servo9.write(pos);
-//  }
-//  delay(2000);
-//  for(pos = 180; pos>=1; pos -= 1) {
-//    servo9.write(pos);
-//  } 
-//  delay(1000); 
+    delay(100);
+    count++;
+    jsy = analogRead(A1);
+    moved = true;
+  } 
+
+  // UP
+  while (jsy > bound2) {
+    y = 1;
+    servo1.write(pos1[0] + forBack[0]*y*count);
+    servo2.write(pos2[0] + forBack[1]*y*count);
+    servo3.write(pos3[0] + forBack[2]*y*count);
+    servo4.write(pos4[0] + forBack[3]*y*count);
+    servo5.write(pos5[0] + forBack[4]*y*count);
+    servo6.write(pos6[0] + forBack[5]*y*count);
+    
+    delay(100);
+    count++;
+    jsy = analogRead(A1);
+    moved = true;
+  }
+
+  // move it back to original plaform position
+  if (moved) {
+    if (x != 0) {
+      while (count >= 0) {
+        servo1.write(pos1[0] + leftRight[0]*x*count);
+        servo2.write(pos2[0] + leftRight[1]*x*count);
+        servo3.write(pos3[0] + leftRight[2]*x*count);
+        servo4.write(pos4[0] + leftRight[3]*x*count);
+        servo5.write(pos5[0] + leftRight[4]*x*count);
+        servo6.write(pos6[0] + leftRight[5]*x*count);
+        delay(100);
+        count--;
+      }
+    }
+
+    else {
+      while (count >= 0) {
+        servo1.write(pos1[0] + forBack[0]*y*count);
+        servo2.write(pos2[0] + forBack[1]*y*count);
+        servo3.write(pos3[0] + forBack[2]*y*count);
+        servo4.write(pos4[0] + forBack[3]*y*count);
+        servo5.write(pos5[0] + forBack[4]*y*count);
+        servo6.write(pos6[0] + forBack[5]*y*count);
+        delay(100);
+        count--;
+      }
+    }
+
+    delay(1000);
+
+    // set them all back to initial position
+    servo1.write(pos1[0]);
+    servo2.write(pos2[0]);
+    servo3.write(pos3[0]);
+    servo4.write(pos4[0]);
+    servo5.write(pos5[0]);
+    servo6.write(pos6[0]);
+    delay(500);
+  }
   
-  servo1.moveCCW(90, 5);
-  delay(5000); // need this
-  servo1.moveCW(90, 5);
-  delay(1000); // need this
-}
-
-// ------------------------------------
-
-MyServo::MyServo(int pin) {
-  m_pin = pin;
-  theServo.attach(m_pin);
-}
-
-void MyServo::moveCCW(int desiredAngle, int speedStep) {
-  for (m_angle = 0; m_angle <= desiredAngle; m_angle += speedStep) {
-    theServo.write(m_angle);
-  }
-//  Serial.println("Move CW and now m_angle is");
-//  Serial.println(m_angle);
-}
-
-void MyServo::moveCW(int desiredAngle, int speedStep) {
-  for (m_angle = desiredAngle; m_angle >= 0; m_angle -= speedStep) {
-    theServo.write(m_angle);
-  }
-//  Serial.println("Move CCW and now m_angle is");
-//  Serial.println(m_angle);
-}
-
-int MyServo::getAngle() {
-  return m_angle;
 }
