c596af0e5e923a1bc9de022cb0c25b3bd280fc72
tracywhchen <twhchen@gmail.com>
2019-11-18 13:33:47 -0500

    :rage: a little progress but still doesn't work... sigh

--- BasicControl.ino
+++ BasicControl.ino
@@ -17,7 +17,7 @@ StewartPlatform sp;
 Servo servos[6];
 float servosPosition[6];
 
-point_t translation = {}, rotation = {};
+point_t translation = {0,0,0}, rotation = {0.1,0,0};
 
 void setup() {
   Serial.begin(115200);  
@@ -36,19 +36,28 @@ float count = 0;
 void loop() {  
   readIMU();
   
-  /*count = count + 1;
+  count = count + 1;
   if(count == 100){
-    translation.y = 10;
+    translation.z = 2;
   }
-  if(count == 500){
-    translation.y = -10;
-    count = 0;
-  }*/
   
+  if(count == 500){
+    translation.z = -2;
+    count = 0;
+  }
+
   sp.getServoPosition(translation, rotation, servosPosition);
-  for(int i = 0; i < 6; i++){      
+  
+  for(int i = 0; i < 6; i++){   
     servos[i].writeMicroseconds(servosPosition[i]);
-  }  
+    
+    if (count == 500) {
+      Serial.print("position of servo "); Serial.print(i); Serial.print(": ");
+      Serial.println(servosPosition[i]); 
+    }
+  }
+
+  delay(5);
 }
 
 int count2 = 0;
@@ -56,6 +65,7 @@ float rawX = 0; float rawY = 0; float rawZ = 0;
 void readIMU(){
   // Read IMU from A0, A1, A2
   count2 = count2 + 1;
+  
   if (count2 % 10 == 0){
     //tempbool = false;
      rawX = analogRead(A0);
@@ -69,32 +79,34 @@ void readIMU(){
      float scaledZ = mapf(rawZ, 0, 1023, -scale, scale);
 
      // Print out raw X,Y,Z accelerometer readings
-  //Serial.print("Y: "); Serial.println(rawY);
-
-  // Print out scaled X,Y,Z accelerometer readings
-  //Serial.print("Y: "); Serial.print(scaledY); Serial.println(" g");
-
-  float accelerationX = scaledX;
-  float accelerationY = scaledY;
-  float accelerationZ = scaledZ;
+//    Serial.print("Y: "); Serial.println(rawY);
   
-  float pitch = 180 * atan (accelerationX/sqrt(accelerationY*accelerationY + accelerationZ*accelerationZ))/PI;
-  float roll = 180 * atan (accelerationY/sqrt(accelerationX*accelerationX + accelerationZ*accelerationZ))/PI;
-  float yaw = 180 * atan (accelerationZ/sqrt(accelerationX*accelerationX + accelerationZ*accelerationZ))/PI;
-
-  float rotX = pitch/360;
-  if (abs(float((rotX-rotation.x)/rotation.x)) < 0.05){
-      rotX = rotation.x;
+    // Print out scaled X,Y,Z accelerometer readings
+    //Serial.print("Y: "); Serial.print(scaledY); Serial.println(" g");
+  
+    float accelerationX = scaledX;
+    float accelerationY = scaledY;
+    float accelerationZ = scaledZ;
+    
+    float pitch = 180 * atan (accelerationX/sqrt(accelerationY*accelerationY + accelerationZ*accelerationZ))/PI;
+    float roll = 180 * atan (accelerationY/sqrt(accelerationX*accelerationX + accelerationZ*accelerationZ))/PI;
+    float yaw = 180 * atan (accelerationZ/sqrt(accelerationX*accelerationX + accelerationZ*accelerationZ))/PI;
+  
+    float rotX = pitch/360;
+    
+    if (abs(float((rotX-rotation.x)/rotation.x)) < 0.05){
+//      rotX = rotation.x;
   }
+  
   if(rotX < 0.1 && rotX > -0.1){
-    rotation.x = rotX/2;
-    Serial.println(rotX/2);
+//    rotation.x = rotX/2;
+//    Serial.println(rotX/2);
   }
   
   }
 }
+
 // Same functionality as Arduino's standard map function, except using floats
-float mapf(float x, float in_min, float in_max, float out_min, float out_max)
-{
+float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
